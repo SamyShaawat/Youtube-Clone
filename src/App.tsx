6b5a83df3b5ed35c75fedfd6a1f0055ec61419d6
Samy Shaawat <samy.shaawat@gmail.com>
2025-09-25 17:49:28 +0300

    create project structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./index.css";
+import { PageHeader } from "./layouts/PageHeader.tsx";
+
+export default function App() {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+    </div>
+  );
+}
