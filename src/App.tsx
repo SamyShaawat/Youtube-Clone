2419a762f043e369512292274c39e8baccdc9c4a
Samy Shaawat <samy.shaawat@gmail.com>
2025-09-29 09:13:55 +0300

    add category pills

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { CategoryPills } from "./components/CategoryPills.tsx";
+import { categories } from "./data/home.ts";
 import "./index.css";
 import { PageHeader } from "./layouts/PageHeader.tsx";
 
@@ -6,10 +7,10 @@ export default function App() {
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div className="grid grid-cols-[auto,1fr] flex-grow overflow-hidden">
-        <div className="">Sidebar</div>
+      <div className="grid grid-cols-[auto,1fr] flex-grow overflow-auto">
+        <div>Sidebar</div>
         <div className="stickey top-0 bg-white z-10 pb-4">
-          <CategoryPills />
+          <CategoryPills categories={categories} />
         </div>
       </div>
     </div>
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,5 +1,22 @@
-export const CategoryPills = () => {
+import { Button } from "./Button.tsx";
+
+type CategoryPillProps = {
+  categories: string[];
+};
+export function CategoryPills({ categories }: CategoryPillProps) {
   return (
-    <div>CategoryPills</div>
-  )
+    <div className="overflow-x-hidden relative">
+      <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
+        {categories.map((category) => (
+          <Button
+            key={category}
+            variant="dark"
+            className="py-1 px-3 rounded-lg whitespace-nowrap"
+          >
+            {category}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
 }
